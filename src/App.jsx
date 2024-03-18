4c6b9928547129a0175c5631b021496337503950
ThalesAuc <thalesaugusto312@gmail.com>
2024-03-18 12:38:50 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+    <form>
+      <label class="form">Nome:<input name="Nome"/></label>
+      <label class="form">CPF:<input name="CPF"/></label>
+      <label class="form">CPF:<input name="CPF"/></label>
+      <label class="form">CPF:<input name="CPF"/></label>
+      <label class="form">CPF:<input name="CPF"/></label>
+      <label class="form">CPF:<input name="CPF"/></label> 
+    </form>
+    </>
+  )
+}
+
+export default App
