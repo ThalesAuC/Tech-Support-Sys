44ca7b28392bbb13e44c4df876ec62973e1fd85f
ThalesAuc <thalesaugusto312@gmail.com>
2024-03-22 01:40:05 -0300

    novo formulario

--- src/App.jsx
+++ src/App.jsx
@@ -5,13 +5,11 @@ function App() {
 
   return (
     <>
-    <form>
+    <form id='cadastroForm'>
       <label class="form">Nome:<input name="Nome"/></label>
       <label class="form">CPF:<input name="CPF"/></label>
-      <label class="form">CPF:<input name="CPF"/></label>
-      <label class="form">CPF:<input name="CPF"/></label>
-      <label class="form">CPF:<input name="CPF"/></label>
-      <label class="form">CPF:<input name="CPF"/></label> 
+      <label class="form">Email:<input name="Email"/></label>
+      <button class="form" type="Submit">Enviar</button>
     </form>
     </>
   )
